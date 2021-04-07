5c44294c1a64edcd1b595b2fdcddc384942278bb
Augusto Emmel Licht <chinchilaushin@gmail.com>
2021-04-07 19:33:08 -0300

    Request de POST backend e junção com front

--- server/src/app.js
+++ server/src/app.js
@@ -2,14 +2,21 @@ import './config/dotenv.js'
 import './config/db.js'
 import cors from 'cors'
 import express from 'express'
+import usuario from './routes/usuarioRotas.js'
 
+//Configurando Portas
 const port = process.env.PORT || 5000;
-const app = express();
 
+//Configurando Middleware e Framework
+const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//Definindo rotas para Middleware
+app.use('/usuario', usuario);
+
+//Definindo Portas
 app.listen(port);
 console.log(`Escutando em http://localhost:${port}/`);
 
