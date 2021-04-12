12ea4dbe21c90f016bb0e73bfa4e6b6580dbfd9b
rodrigo furlani <rodrigo_rpf_furlani@hotmail.com>
2021-04-11 22:05:52 -0300

    asd

--- front/src/main.js
+++ front/src/main.js
@@ -3,7 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 import { VueMaskDirective } from "v-mask";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
+Vue.use(VueAxios, axios);
 Vue.directive("mask", VueMaskDirective);
 
 Vue.config.productionTip = false;
