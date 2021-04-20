e1034c574227b3f466b8616d72b4f240d90cef95
Augusto Emmel Licht <chinchilaushin@gmail.com>
2021-04-20 16:13:53 -0300

    Metodos

--- server/src/routes/usuarioRotas.js
+++ server/src/routes/usuarioRotas.js
@@ -3,7 +3,9 @@ import { Router } from 'express';
 
 //Importa controller
 import {
-    cadastrar
+    cadastrar,
+    logar,
+    getUsuario
 } from '../controllers/usuarioController.js'
 
 //Inicializa o router
@@ -11,6 +13,8 @@ const router = Router();
 
 //Chama as rotas utilizando as funções do controller
 router.route('/cadastro').post(cadastrar);
+router.route('/login').post(logar);
+router.route('/usuario').post(getUsuario);
 
 //Exporta o router
 export default router;
