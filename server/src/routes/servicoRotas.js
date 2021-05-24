45870acb6b4635415cdf774edf33195903dd1fba
Augusto Emmel Licht <chinchilaushin@gmail.com>
2021-05-24 19:48:56 -0300

    Correcoes

--- server/src/routes/servicoRotas.js
+++ server/src/routes/servicoRotas.js
@@ -14,8 +14,8 @@ import {
 const router = Router();
 
 //Chama as rotas utilizando as funções do controller
-router.route('/cadastro').post(cadastrar);
-router.route('/listarServicos').get(listarServicos);
+router.route('/add').post(cadastrar);
+router.route('/select').get(listarServicos);
 router.route('/:id').get(dadosServico);
 router.route('/:id').put(atualizarServico);
 router.route('/:id').delete(deletarServico);
