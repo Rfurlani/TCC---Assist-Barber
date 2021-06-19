6823dbb5017adfb6828cd856d1c410748b215e01
Augusto Emmel Licht <chinchilaushin@gmail.com>
2021-06-18 22:37:23 -0300

    cors config

--- server/src/utils/cors-config.js
+++ server/src/utils/cors-config.js
@@ -3,7 +3,7 @@ import { REQ_PORT } from "../constants";
 const corsOptions = {
     origin: REQ_PORT,
     credentials: true,
-    allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept",
+    allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept, Authorization",
     methods: "GET, POST, PUT, PATCH, DELETE"
 };
 
