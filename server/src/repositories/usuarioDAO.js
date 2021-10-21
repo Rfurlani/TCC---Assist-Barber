11ea8dd80be4263915236e7b14dc0e01b384fdd4
Augusto Emmel Licht <augusto.emmel@gmail.com>
2021-10-21 10:55:21 -0300

    busca validacao ativos

--- server/src/repositories/usuarioDAO.js
+++ server/src/repositories/usuarioDAO.js
@@ -19,7 +19,7 @@ class UsuarioDAO {
     }
 
     buscarBarbeirosNaoValidados(){
-        return this.model.find( {cargo:'barbeiro', validado:false} ).exec();
+        return this.model.find( {cargo:'barbeiro', validado:false, ativo: true} ).exec();
     }
 
     async buscarPorEmail(email) {
